c6130b346f43076e5582ef4e2e311b5f30a0052b
Debjit Mukherjee <72387674+Debjit337@users.noreply.github.com>
2020-10-08 03:16:22 +0530

    Add files via upload

---
+++ str work.py
@@ -0,0 +1,25 @@
+Biodata = """
+---------------------------------------------------
+                    Biodata
+---------------------------------------------------
+Name : Debjit Mukherjee
+DOB : 28/3/97
+Address : Kalna,Burdwan.
+Gender : Male
+Nationality : Indian                 
+---------------------------------------------------
+"""
+print(Biodata)
+
+name = 'Debjit'
+print('\nA.', name,'. B.'' C.')
+
+# use concatation '+' in stirng
+info = 'Friend name=, ' + name
+# print(info)
+Friend = 'A.'
+Friend2 = 'B.'
+Friend3 = 'C.'
+# show welcome message
+print(info + '\n' + Friend + '\n' + Friend2 + '\n' + Friend3)
+
