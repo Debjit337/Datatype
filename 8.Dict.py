5179f398ad80050505788ed3b7ddf433b35a6ea1
Debjit Mukherjee <72387674+Debjit337@users.noreply.github.com>
2020-10-15 20:43:43 +0530

    Add files via upload

---
+++ 8.Dict.py
@@ -0,0 +1,56 @@
+'''
+Dictionary in Python:
+About Assosiative Array:
+1. C, C++ ya php me hame assosiative array milta hai.
+2. Synatx: variableName = array('name' => 'Sushil')
+3. Assosiatve sysntax me array(key => value)
+4. Yahi same concept of associative array python me Dictionary ke name se use hota hai.
+Python me dictionary ko use karne ke liye:
+1. Syntax: newvalue = {'key or userDefinedName' : 'value or userDefinedValue',}
+2. Tupel me () small bracke ke agar koi single value define ki jaye to wo tuple type nahi hai because tuple ke liye last me comma dena jaruri hai.
+3. Waise hi dictionay me {} ke andar key:value ke bad comma dena jaruri hai. Other wise same braket ka use set me bhi hota hai. Aur dono confilt ho jata hai.
+'''
+
+data = {}
+print('\nData type: ', type(data))
+
+# data = {key : value}
+data = {'name' : 'Nikhil', 'mobile' : 987987987, 'course' : 'Python'}
+
+print('\nName: ', data['name'])
+
+profile = '''
+--------------------------------
+            {name}'s Profile
+--------------------------------
+Name: {name}
+Mobile: {mobile}
+Course: {course}
+--------------------------------            
+'''.format(name = data['name'],mobile = data['mobile'],course = data['course'])
+# .format(data['name'], data['mobile'], data['course'])
+
+print(profile)
+
+# multi level dictionary
+megadata = {
+    'nikhil' : {
+        'name' : 'Nikhil',
+        'mobile' : 987987987,
+        'course' : 'Python'
+    },
+    'debjit' : {
+        'name' : 'Debjit',
+        'mobile' : 9987987979
+    }
+}
+print('\nMobile: ', megadata['debjit']['mobile'])
+
+print('\nGet only keys: ', megadata.keys())
+print('\nGet only values: ', megadata.values())
+# print('\nPopitem: ', megadata.popitem())
+print('\nPop: ', megadata.pop('nikhil'))
+
+# megadata
+
+print('\nData: ', megadata)
