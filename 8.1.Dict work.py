e9ece8f7ccac8cd19f567b8455397d706a39338e
Debjit Mukherjee <72387674+Debjit337@users.noreply.github.com>
2020-10-15 21:45:23 +0530

    Add files via upload

---
+++ 8.1.Dict work.py
@@ -0,0 +1,32 @@
+Data={'name' : 'Debjit','mobile' : '789456123','course' : 'Python','address' : 'Kalna'}
+
+profile=''' 
+-------------------------------
+        {name}'s Profile
+-------------------------------
+Name: {name}
+Mobile: {mobile}
+Course: {course}
+Address: {address}
+-------------------------------
+'''.format(name=Data['name'],mobile=Data['mobile'],course=Data['course'],address=Data['address'])
+print(profile)
+data = {
+    'nikhil' : {
+        'name' : 'Nikhil',
+        'mobile' : 987987987,
+        'course' : 'Python',
+        'address': 'Delhi'
+    },
+    'debjit' : {
+        'name' : 'Debjit',
+        'mobile' : 9987987979,
+        'course' : 'Python',
+        'address': 'Kalna'
+    }
+}
+print(data)
+print('\nName:',data['debjit'] ['name'])
+print('\nMobile:',data['debjit'] ['mobile'])
+print('\nCourse:',data['debjit'] ['course'])
+print('\nAddress:',data['debjit'] ['address'])
